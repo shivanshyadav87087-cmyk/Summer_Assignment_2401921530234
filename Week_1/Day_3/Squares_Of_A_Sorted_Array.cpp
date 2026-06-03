a3e86674cdb02f98880e3ce82ec55165d1c04eb1
shivanshyadav87087-cmyk <shivanshyadav87087@gmail.com>
2026-06-03 14:06:03 +0530

    Add solution for sorted squares of a sorted array
    Implemented both brute force and optimal approaches for sorting squares of a sorted array.

---
+++ Week_1/Day_3/Squares_Of_A_Sorted_Array.cpp
@@ -0,0 +1,38 @@
+//Bruteforce approach
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            nums[i]=nums[i]*nums[i];
+        }
+        sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
+// optimal approach
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>result(n);
+        int i=0;
+        int j=n-1;
+        int k=n-1;
+        while(k>=0){
+            int a =nums[i]*nums[i];
+            int b=nums[j]*nums[j];
+
+            if(a>b){
+                result[k]=a;
+               i++;
+            }
+            else{
+                result[k]=b;
+                j--;
+            }
+            k--;
+        }
+        return result;
+    }
+};
