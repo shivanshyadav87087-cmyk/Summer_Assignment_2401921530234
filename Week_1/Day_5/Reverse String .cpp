6d79abde046ea04b985a865e2402a49ce9127470
Shivansh yadav <shivanshyadav87087@gmail.com>
2026-06-05 13:24:35 +0530

    Add Reverse String solution implementations

---
+++ Week_1/Day_5/Reverse String .cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+       int i=0;
+       int j=s.size()-1;
+       while(i<j){
+        swap(s[i],s[j]);
+        i++;
+        j--;
+       }
+    }
+};
+//
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        reverse(s.begin(),s.end());
+    }
+};
